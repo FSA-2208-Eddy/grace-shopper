651d5ae68c886ef1a38ef5f649e1c0779d088966
Thomas Bak <tombak98@gmail.com>
2022-09-19 14:38:59 -0400

    changed database name to shopper

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/test', { logging: false });
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/shopper', { logging: false });
 
 module.exports = db;
 
