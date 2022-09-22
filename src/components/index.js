d340dd8667a31b2312aa193b7c0cf2e1e2ea2845
Thomas Bak <tombak98@gmail.com>
2022-09-22 14:48:32 -0400

    finished footer

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as eventSlice } from "../store/events/eventSlice";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as EventList } from "./EventList";
 export { default as LoginPage } from "./login/LoginPage";
+export { default as Footer } from './Footer';
