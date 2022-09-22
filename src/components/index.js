17b3849880dead8c467c727342f9c123fc6cc54c
Thomas Bak <tombak98@gmail.com>
2022-09-22 14:51:48 -0400

    cleaned up file structure:

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as userSlice } from "../store/users/userSlice";
 export { default as eventSlice } from "../store/events/eventSlice";
 export { default as Navbar } from "./navbar/Navbar";
-export { default as EventList } from "./EventList";
+export { default as EventList } from "./eventsList/EventList";
 export { default as LoginPage } from "./login/LoginPage";
-export { default as Footer } from './Footer';
+export { default as Footer } from './footer/Footer';
