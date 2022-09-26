29996bd4dfacdded9024f88c02fafd465589d2c8
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-09-26 12:17:07 +0100

    Test list of api calls created and saved in /db/apiCalls.js

---
+++ server/db/apiCalls.js
@@ -0,0 +1,80 @@
+
+
+
+const fullSeed = [{
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=new%20york&locale=*&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=new%20york&locale=*&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=new%20york&locale=*&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=new%20york&locale=*&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=los%20angeles&locale=*&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=los%20angeles&locale=*&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=los%20angeles&locale=*&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&keyword=los%20angeles&locale=*&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=2&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=3&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=4&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=5&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=6&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=7&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=8&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=9&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=IE&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=IE&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=IE&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=IE&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=GB&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=GB&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=GB&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=GB&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=AU&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=AU&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=AU&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=AU&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=CA&segmentId=KZFzniwnSyZfZ7v7nE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=CA&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=CA&segmentId=KZFzniwnSyZfZ7v7n1`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=CA&segmentId=KZFzniwnSyZfZ7v7na`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=FR`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=FI`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=US&page=2&segmentId=KZFzniwnSyZfZ7v7nJ`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=SE`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=DE`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=IT`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=BE`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=ES`
+    },
+    {
+        1: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=MX`,
+        2: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=MX`,
+        3: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=MX`,
+        4: `https://app.ticketmaster.com/discovery/v2/events?apikey=rybaSZbAsGTyVHpT4MjpWMbbiJIQpYGD&locale=*&countryCode=MX`
+    },
+]
+
+
+
+
+// const segmentId = {
+//     'sports': `KZFzniwnSyZfZ7v7nE`,
+//     'music': `KZFzniwnSyZfZ7v7nJ`,
+//     'misc': `KZFzniwnSyZfZ7v7n1`,
+//     'arts&Theatre': `KZFzniwnSyZfZ7v7na`,
+// }
