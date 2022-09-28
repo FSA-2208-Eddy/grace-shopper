628262d800cfc959fa1e7558f1f1c8435ff9de02
Thomas Bak <tombak98@gmail.com>
2022-09-28 15:13:38 -0400

    whatever

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/shopper', { logging: false, ssl: {rejectUnauthorized: false} });
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/shopper', { logging: false });
 
 module.exports = db;
 
